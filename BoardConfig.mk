33955efa2c4a82f5d9a8bb69f12efeea0fb91548
weivincewang <wvw@google.com>
2021-01-03 14:30:30 +0000

    sagit: Use mke2fs to create ext4 images
    Set TARGET_USES_MKE2FS := true to use new EXT4 tool chain
    
    Bug: 62421233
    Change-Id: Id6e5c68b2d7854e5330ad87443b974361a4bff9d
    Signed-off-by: NurKeinNeid <mralexman3000@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -88,6 +88,7 @@ BOARD_USERDATAIMAGE_FILE_SYSTEM_TYPE := ext4
 
 TARGET_USERIMAGES_USE_EXT4 := true
 TARGET_USERIMAGES_USE_F2FS := true
+TARGET_USES_MKE2FS := true
 
 # Workaround for error copying vendor files to recovery ramdisk
 BOARD_VENDORIMAGE_FILE_SYSTEM_TYPE := ext4
